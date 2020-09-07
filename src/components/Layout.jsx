e9651a0b4afc8d62065e2d8b17049f006877f557
Jeff Kinley <jkinley@mac.com>
2020-09-07 15:23:07 -0700

    ./header bug fix

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+import Header from "./Header"
 import Footer from "./Footer"
 import '../css/index.css'
 import "./layout.css"
