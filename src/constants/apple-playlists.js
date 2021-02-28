aba02565ed443a88029fd5a06c9b8a38fe07de36
Jeff Kinley <jkinley@mac.com>
2021-02-28 09:27:29 -0800

    remove placeholder playlist

--- src/constants/apple-playlists.js
+++ src/constants/apple-playlists.js
@@ -6,16 +6,11 @@ export default [
   },
   {
     id: 2,
-    title: "Sammy Cahn-Jimmy Van Heusen Songbook",
-    url: "https://embed.music.apple.com/us/playlist/frank-sinatra-the-sammy-cahn-jimmy-van-heusen-songbook/pl.898dc92aa9334bc4aff8154a079f6a7b"
-  },
-  {
-    id: 3,
     title: "Torch Songs",
     url: "https://embed.music.apple.com/us/playlist/sammy-cahn-torch-songs/pl.u-Xv5LtoNKGR"
   },
   {
-    id: 4,
+    id: 3,
     title: "Holiday Songs",
     url: "https://embed.music.apple.com/us/playlist/holiday-songs/pl.u-qk76T8l5p0"
   }
