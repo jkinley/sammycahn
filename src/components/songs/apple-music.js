77f3c095c6469f4e1a98bb63811a60286b086e09
Jeff Kinley <jkinley@mac.com>
2021-02-28 09:20:25 -0800

    update apple playlist component, add torch songs

--- src/components/songs/apple-music.js
+++ src/components/songs/apple-music.js
@@ -1,17 +1,41 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import applePlaylists from '../../constants/apple-playlists';
 
 const AppleMusic = () => {
   return (
     <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4">  
-      <iframe title="am-1" allow="autoplay *; encrypted-media *; fullscreen *" frameBorder="0" height="320" style={{width: '100%'}} sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/playlist/sammy-cahn-songbook/pl.237cc45fc0e641e99e8943e4a8173fc2"></iframe>
+      {
+        applePlaylists.map((playlist, id) => {
+          return (
 
-      <iframe title="am-2" allow="autoplay *; encrypted-media *; fullscreen *" frameBorder="0" height="320" style={{width: '100%'}} sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/playlist/frank-sinatra-the-sammy-cahn-jimmy-van-heusen-songbook/pl.898dc92aa9334bc4aff8154a079f6a7b"></iframe>
-
-      <iframe title="am-3" allow="autoplay *; encrypted-media *; fullscreen *" frameBorder="0" height="320" style={{width: '100%'}} sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/playlist/frank-sinatra-the-sammy-cahn-jule-styne-songbook/pl.e7ddc3dd54d64e29b9438a97d7d27dcb"></iframe>
-
-      <iframe title="am-4" allow="autoplay *; encrypted-media *; fullscreen *" frameBorder="0" height="320" style={{width: '100%'}} sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/playlist/holiday-songs/pl.u-qk76T8l5p0"></iframe>
+            <div className="flex flex-col" key={playlist.id}>
+              <Iframe
+                className="mb-6"
+                title={`Apple Music Playlist ${playlist.id}`}
+                src={playlist.url}
+                height="320"
+                frameBorder="0"
+                allow="autoplay *; encrypted-media *; fullscreen *" 
+                sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" 
+                >
+              </Iframe>
+              <h3 className="mb-2 text-sm flex-1">{playlist.title}</h3>
+              <p className="text-sm flex-1">Short description of playlist.</p>
+            </div>
+          )
+        })
+      }
     </div>
   )
-}
+};
 
-export default AppleMusic
+const Iframe = styled.iframe`
+  width: 100%;
+  max-width: 660px;
+  overflow: hidden;
+  border-radius: 10px;
+  background: transparent;
+`;
+
+export default AppleMusic;
---
+++ src/constants/apple-playlists.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 1,
+    title: "Sammy Cahn Songbook",
+    url: "https://embed.music.apple.com/us/playlist/sammy-cahn-songbook/pl.237cc45fc0e641e99e8943e4a8173fc2"
+  },
+  {
+    id: 2,
+    title: "Sammy Cahn-Jimmy Van Heusen Songbook",
+    url: "https://embed.music.apple.com/us/playlist/frank-sinatra-the-sammy-cahn-jimmy-van-heusen-songbook/pl.898dc92aa9334bc4aff8154a079f6a7b"
+  },
+  {
+    id: 3,
+    title: "Torch Songs",
+    url: "https://embed.music.apple.com/us/playlist/sammy-cahn-torch-songs/pl.u-Xv5LtoNKGR"
+  },
+  {
+    id: 4,
+    title: "Holiday Songs",
+    url: "https://embed.music.apple.com/us/playlist/holiday-songs/pl.u-qk76T8l5p0"
+  }
+];
