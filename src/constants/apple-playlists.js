330f992d3fe58a68d4d05dfe007462b7ff01ba02
Jeff Kinley <jkinley@mac.com>
2021-02-28 17:04:33 -0800

    add biggest hits playlist

--- src/constants/apple-playlists.js
+++ src/constants/apple-playlists.js
@@ -1,22 +1,22 @@
 export default [
   {
     id: 1,
-    title: "Sammy Cahn Songbook",
-    url: "https://embed.music.apple.com/us/playlist/sammy-cahn-songbook/pl.237cc45fc0e641e99e8943e4a8173fc2"
-  },
-  {
-    id: 2,
     title: "Torch Songs",
     url: "https://embed.music.apple.com/us/playlist/sammy-cahn-torch-songs/pl.u-Xv5LtoNKGR"
   },
   {
+    id: 2,
+    title: "Love Songs",
+    url: "https://embed.music.apple.com/us/playlist/sammy-cahn-love-songs/pl.u-yENrHDWrv9"
+  },
+  {
     id: 3,
-    title: "Holiday Songs",
-    url: "https://embed.music.apple.com/us/playlist/holiday-songs/pl.u-qk76T8l5p0"
+    title: "Biggest Hits",
+    url: "https://embed.music.apple.com/us/playlist/sammy-cahn-biggest-hits/pl.u-zla9FEdglk"
   },
   {
     id: 4,
-    title: "Love Songs",
-    url: "https://embed.music.apple.com/us/playlist/sammy-cahn-love-songs/pl.u-yENrHDWrv9"
-  }
+    title: "Holiday Songs",
+    url: "https://embed.music.apple.com/us/playlist/holiday-songs/pl.u-qk76T8l5p0"
+  },
 ];
