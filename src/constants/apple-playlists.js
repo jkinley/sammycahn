8ff7dd4f5c186247c2c0172ed731659f8c1c5ccd
Jeff Kinley <jkinley@mac.com>
2021-03-07 09:46:48 -0800

    add jazz and broadway playlists

--- src/constants/apple-playlists.js
+++ src/constants/apple-playlists.js
@@ -21,6 +21,16 @@ export default [
   },
   {
     id: 5,
+    title: "Jazz Versions",
+    url: "https://embed.music.apple.com/us/playlist/sammy-cahn-jazz-versions/pl.u-4a99hdjYbG"
+  },
+  {
+    id: 6,
+    title: "Songs from Broadway",
+    url: "https://embed.music.apple.com/us/playlist/sammy-cahn-songs-from-broadway/pl.u-qEkJS8l5p0"
+  },
+  {
+    id: 7,
     title: "Holiday Songs",
     url: "https://embed.music.apple.com/us/playlist/holiday-songs/pl.u-qk76T8l5p0"
   }
