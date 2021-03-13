1781b22d63e555f12acb06a1e215beac1f6bab2e
Jeff Kinley <jkinley@mac.com>
2021-03-13 09:04:05 -0800

    add spotify playlists

--- src/constants/spotify-playlists.js
+++ src/constants/spotify-playlists.js
@@ -1,14 +1,37 @@
 export default [
-  
   {
-    id: 7,
+    id: 1,
     title: "Torch Songs",
     url: "https://open.spotify.com/embed/playlist/3pLsLYZSDXp6NoCwHkKzBF"
   },
   {
-    id: 8,
+    id: 2,
+    title: "Love Songs",
+    url: "https://open.spotify.com/embed/playlist/2tgstn0GqxfrUiemFYOnGG"
+  },
+  {
+    id: 3,
+    title: "Biggest Hits",
+    url: "https://open.spotify.com/embed/playlist/1b3HGJeGksBfA361LDCKes"
+  },
+  {
+    id: 4,
+    title: "Title Songs",
+    url: "https://open.spotify.com/embed/playlist/1BHsJtE3CDsdeUaiDnxay2"
+  },
+  {
+    id: 5,
+    title: "Jazz Versions",
+    url: "https://open.spotify.com/embed/playlist/3S5Prz6JEfFHd1X6MagbXP"
+  },
+  {
+    id: 6,
+    title: "Songs from Broadway",
+    url: "https://open.spotify.com/embed/playlist/2GjiGu5pcMoKIkIXZUD3Zb"
+  },
+  {
+    id: 7,
     title: "Holiday Songs",
     url: "https://open.spotify.com/embed/playlist/5iX34I2uOxkh9RrFWRh32L"
   },
-  
 ]
