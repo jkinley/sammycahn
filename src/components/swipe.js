adeb2aa4bfc3d00869c73f25b8660c4263598dca
Jeff Kinley <jkinley@mac.com>
2021-05-29 14:31:37 -0700

    quote slide updates

--- src/components/swipe.js
+++ src/components/swipe.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { quotes } from '../constants/quotes';
 import styled from 'styled-components';
 import { FaQuoteLeft } from 'react-icons/fa';
-import SwiperCore, { Navigation, Pagination } from 'swiper';
+import SwiperCore, { Navigation, Pagination } from 'swiper/core';
 import { Swiper, SwiperSlide} from 'swiper/react';
 import "swiper/swiper-bundle.min.css";
+import "swiper/components/effect-fade/effect-fade.min.css"
+import "swiper/components/navigation/navigation.min.css"
 import '../../src/styles/swiper.css';
 
 SwiperCore.use([Navigation, Pagination]);
@@ -14,11 +16,11 @@ const Swipe = () => {
     const slides = quotes.map((quote) => {
       return (
         <SwiperSlide key={quote.id}>
-          <Blockquote className="bg-brand-darkgold rounded-md">
-            <FaQuoteLeft className="icon"/>
+          <Blockquote className="">
+            <FaQuoteLeft className="text-3xl"/>
             <div>
-              <p className="quote mb-8">{quote.text}</p>
-              <p className="author">— {quote.author}</p>
+              <p className="mb-8 text-3xl leading-normal">{quote.text}</p>
+              <p className="text-xl author">— {quote.author}</p>
             </div>
             
           </Blockquote>
@@ -27,56 +29,36 @@ const Swipe = () => {
     });
 
     return (
-      <section className="relative bg-brand-gold py-12">
-        <Swiper
-          className=""
-          id="home" 
-          navigation={{
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev'
-          }}
-          loop={true}
-          spaceBetween={30}
-          breakpoints={breakpoints}
-        >
-          {slides}
-          
-        </Swiper>
-        <div className="swiper-button-prev"></div>
-        <div className="swiper-button-next"></div>
-        
+      <section className="relative bg-brand-gold p-16">
+        <div className="container relative mx-auto">
+          <Swiper
+            className=""
+            id="home" 
+            navigation={{
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev'
+            }}
+            loop={true}
+            spaceBetween={30}
+            slidesPerView={1}
+          >
+            {slides}
+            
+          </Swiper>
+          <div className="swiper-button-prev"></div>
+          <div className="swiper-button-next"></div>
+        </div>
       </section>
     );
 };
 
-const breakpoints = {
-    640: {
-      slidesPerView: 1,
-    },
-    1024: {
-      slidesPerView: 2,
-    },
-    1280: {
-      slidesPerView: 3,
-    }
-};
-
 const Blockquote = styled.blockquote`
 	display: grid;
-  padding: 2rem;
-  align-content: center;
   height: 100%;
+  align-content: center;
+  justify-content: center;
 	grid-gap: 1rem;
 	grid-template-columns: auto 1fr;
-  & .icon {
-    font-size: 2rem;
-  }
-  & .quote {
-    font-size: 1.1rem;
-  }
-  & .author {
-    font-weight: bold;
-  }
 `;
 
 export default Swipe;
--- src/constants/quotes.js
+++ src/constants/quotes.js
@@ -29,14 +29,20 @@ export const quotes = [
 		text: "Of all the writers, Sammy's words fit my mouth the best.",
 		author: "Frank Sinatra"
 	},
-	// {
-	// 	id: 7,
-	// 	text: "Sammy Cahn is one of the few lyricists whose every line gave the melody added power, beauty and resonance. I’m not saying the tunes wouldn’t have made it without him but they wouldn’t have had the same emotional impact, wit and wisdom, sometimes happy, sometimes sad, that Sammy imparted to every song he graced. Just ask some of the greatest singers of popular song that ever lived, who were lucky enough to benefit from his thoughts and humour.",
-	// 	author: "Sir Tim Rice"
-	// },
+	{
+		id: 7,
+		text: "Sammy Cahn is one of the few lyricists whose every line gave the melody added power, beauty and resonance. I’m not saying the tunes wouldn’t have made it without him but they wouldn’t have had the same emotional impact, wit and wisdom—sometimes happy, sometimes sad—that Sammy imparted to every song he graced.",
+		author: "Sir Tim Rice"
+	},
 	{
 		id: 8,
 		text: "I'm a professional songwriter—personal attitudes have nothing to do with writing a song.",
 		author: "Sammy Cahn"
+	},
+  {
+		id: 9,
+		text: "Sammy was generous with his talent, writing special lyrics to benefit endless worthy causes. Funny, touching, sometimes R rated! It was a special honor to be the target of his razor sharp and lightning quick wit. A delicious attack, his words like rubber bullets providing an edge to camouflage endless acts of kindness.",
+		author: "Paul Williams"
 	}
-]
+];
+
