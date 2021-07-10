548922964ed48d5fa82edfcaaa141e15fe42f0c4
Jeff Kinley <jkinley@mac.com>
2021-07-10 11:45:56 -0700

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,15 @@ module.exports = {
 				name: `data`,
 				path: `${__dirname}/src/data`,
 			},
-		},
+		},{
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-6T0L6VSW77", // Google Analytics / GA
+        ],
+      },
+    },
 		{
 			resolve: `gatsby-plugin-manifest`,
 			options: {
